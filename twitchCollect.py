0c84cc4b0ca7f8a2ab7bdf2c7f0c2afa1a216d22
zcochran <fuzzylimes@gmail.com>
2017-12-15 21:58:31 -0500

    Added support for game "caching" to reduce the number of requests per scrape.

--- twitchCollect.py
+++ twitchCollect.py
@@ -5,16 +5,32 @@ import newTwitchClient as NTC
 with open('streams.json') as json_data:
     channels = json.load(json_data)
 
+with open('games.json') as game_data:
+    games = json.load(game_data)
+
+rec_length = len(games)
+
 twitch = NTC.TwitchApi()
 for stream in channels['channels']:
+    print("Collecting " + stream)
     status = twitch.GetChannelInfo(login=stream)
     if not "response" in status:
         status = status['data'][0]
+        game_id = status['game_id']
+
+        # Check to see if the game is already in our saved games list
+        if game_id in games:
+            game_name = games[game_id]['data'][0]['name']
+        else:
+            game_res = twitch.GetGames(gid=game_id)
+            games[game_id] = game_res.json()
+            game_name = game_res.json()['data'][0]['name']
+        
         info = {
             "channel_name": stream,
             "channel_id": status['user_id'],
-            "game_id": status['game_id'],
-            "game_name": twitch.GetGames(gid="{}".format(status['game_id'])).json()['data'][0]['name'],
+            "game_id": game_id,
+            "game_name": game_name,
             "viewers": status['viewer_count'],
             "started": status['started_at'],
             "chatters": twitch.GetChatters(stream)['chatter_count'],
@@ -45,3 +61,9 @@ for stream in channels['channels']:
         if not file_exists:
             writer.writeheader()
         writer.writerow(info)
+    
+    print("Finished " + stream)
+
+if len(games) > rec_length:
+    with open('games.json', 'w') as data_file:
+        json.dump(games, data_file)
