bf3c9786faf11f3acc6f8de8042c11b291c0ab7a
zcochran <fuzzylimes@gmail.com>
2017-12-17 20:54:59 -0500

    Moving file to new sub folder

---
+++ Utils/fixcsv2.py
@@ -0,0 +1,27 @@
+import os
+import csv
+import shutil
+from tempfile import NamedTemporaryFile
+import datetime
+
+UTC_OFFSET_TIMEDELTA = datetime.datetime.utcnow() - datetime.datetime.now()
+
+for file in os.listdir("../csv/"):
+    print(file)
+    if file.endswith(".csv"):
+        tempfile = NamedTemporaryFile(mode="w", delete=False)
+        with open('../csv/'+file, 'r') as csvfile, tempfile:
+            reader = csv.reader(csvfile)
+            writer = csv.writer(tempfile)
+
+            # Handle headers
+            header = reader.__next__()
+            writer.writerow(header)
+
+            for row in reader:
+                if not row[0].endswith("Z"):
+                    string_date = datetime.datetime.strptime(row[0], "%Y-%m-%dT%H:%M:%S.%f")
+                    string_date = string_date + UTC_OFFSET_TIMEDELTA
+                    row[0] = string_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+                writer.writerow(row)
+        shutil.move(tempfile.name, '../csv/'+file)
---
+++ Utils/gamescan.py
@@ -0,0 +1,28 @@
+import os, sys
+import csv
+import json
+from tempfile import NamedTemporaryFile
+import datetime
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import newTwitchClient as NTC
+
+with open('../games.json') as game_data:
+    games = json.load(game_data)
+
+twitch = NTC.TwitchApi()
+
+for file in os.listdir("../csv/"):
+    print(file)
+    if file.endswith(".csv"):
+        with open('../csv/'+file, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            # Handle headers
+            header = reader.__next__()
+            for row in reader:
+                if row[3] != "NULL":
+                    if not row[3] in games:
+                        new_game = twitch.GetGames(gid=row[3])
+                        games[row[3]] = new_game.json()
+
+with open('../games.json', 'w') as data_file:
+        json.dump(games, data_file)
